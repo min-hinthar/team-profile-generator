7b52bdc7d0f4d3de4ad99841a898e8dbad96adf5
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-07-08 03:27:32 -0700

    index.js updated, require inquirer, import constructor classes and methods, created new empty array to store user input data, created parent constructor class Employee, extended to child classes Manager, Engineer, Intern

---
+++ index.js
@@ -0,0 +1,12 @@
+// require inquirer
+const inquirer = require('inquirer')
+
+// import Manager class
+const Manager = requrie('./lib/Manager')
+// impor Engineer class
+const Engineer = requrie('./lib/Engineer')
+// import Intern class
+const Inter = requrie('./lib/Intern')
+
+// create new empty array for data to be written as Team Profile
+const teamProfile = []
